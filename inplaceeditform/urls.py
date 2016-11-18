c8a784542be0ba182d03929b1409e7d7190b8df3
Paul Stenius <stenius@gmail.com>
2016-11-18 13:51:52 -0600

    remove url.patterns import for django 1.10

--- inplaceeditform/urls.py
+++ inplaceeditform/urls.py
@@ -16,7 +16,7 @@
 from inplaceeditform.views import save_ajax, get_field
 
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:  # Django < 1.4
     from django.conf.urls.defaults import patterns, url
 
